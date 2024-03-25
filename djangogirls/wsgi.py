b722093eee87b36f341dd01a1866cfe3a095c219
ArisaJeong <jsumin02@icloud.com>
2024-03-25 19:44:00 +0900

    WSGI Update! (TimeZone)

--- djangogirls/wsgi.py
+++ djangogirls/wsgi.py
@@ -6,10 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
+import time
 
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ["TZ"] = "Asia/Seoul"
+time.tzset()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangogirls.settings')
 
